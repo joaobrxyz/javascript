6f5bea3a4027f443a1504aac20981e6d2a0b6f28
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-02-08 16:37:07 -0300

    Cont. do input

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,7 +1,20 @@
+function valor(select, valor) {
+    let item = select.options
+    for (let c = 0; c < item.length; c++) {
+        if (item[c].value === valor.value) {
+            return true
+        }
+    }
+    return false
+}
 function add() {
     let num = Number(document.getElementById('num').value)
     let list = document.getElementById('list')
     let item = document.createElement('option')
     item.text = `Valor ${num} adicionado.`
-    list.appendChild(item)
+    if (valor(list, item) == true) {
+        alert('Número já adicionado!')
+    } else {
+        list.appendChild(item)
+    }
 }
