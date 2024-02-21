5f8fd054e932e9ea34aa5dde2a94f023202fe788
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-02-21 19:15:36 -0300

    Create operador ternário.js

---
+++ aula06 (Tratamento de dados)/operador ternário.js
@@ -0,0 +1,10 @@
+let num1 = 10
+let num2 = 5
+res = (!(num1%2) ? "Par" : "Impar") // Nesse caso foi necessário inverter o resultado true/false
+res2 = (num1 > num2 ? 'Verdadeiro' : 'Falso')
+console.log(res, res2)
+
+//0 = False
+//1 = True
+
+// Teste lógico ? se verdadeiro : se falso
