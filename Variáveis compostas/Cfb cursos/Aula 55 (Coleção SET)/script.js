81653288a748ca349ec4166f5a878d42f21acff0
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-07-17 20:19:26 -0300

    Aula 55

---
+++ Variáveis compostas/Cfb cursos/Aula 55 (Coleção SET)/script.js
@@ -0,0 +1,21 @@
+// Set não permite entrada duplicada
+const caixa = document.querySelector("#caixa")
+
+let musicas = new Set(["musica 1","musica boa","musica 10"])
+
+musicas.add("musica muito legal")
+musicas.add("musica 1")
+musicas.add("musica 1")
+musicas.add("musica 10")
+
+musicas.delete("musica1")
+
+console.log(musicas)
+
+musicas.forEach((el)=>{
+    caixa.innerHTML += `${el} <br/>`
+})
+
+for (let m of musicas){
+    console.log(m)
+}
