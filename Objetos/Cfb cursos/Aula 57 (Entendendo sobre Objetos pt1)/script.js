430402d1bec908521db04ddf44de89f54fb56797
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-07-18 20:39:04 -0300

    Aula 57

---
+++ Objetos/Cfb cursos/Aula 57 (Entendendo sobre Objetos pt1)/script.js
@@ -0,0 +1,12 @@
+class Pessoa {
+    constructor(pnome){
+        this.nome = pnome
+    }
+}
+let p1 = new Pessoa("João")
+let p2 = new Pessoa("Asdervardson")
+let p3 = new Pessoa("Bernasclartel")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
