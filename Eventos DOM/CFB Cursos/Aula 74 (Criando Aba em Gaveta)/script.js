2b59d1e54464fcb68f154bb850013b837457eb5c
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-07-27 14:03:34 -0300

    Aula 74

---
+++ Eventos DOM/CFB Cursos/Aula 74 (Criando Aba em Gaveta)/script.js
@@ -0,0 +1,72 @@
+const teclasNum = [...document.querySelectorAll(".num")]
+const teclasOp = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelector("#res")
+const display = document.querySelector(".display")
+const tcopy = document.querySelector("#tcopy")
+const tlimpar = document.querySelector("#tlimpar")
+const tigual = document.querySelector("#tigual")
+const calc_aba = document.querySelector("#calc_aba")
+const calc = document.querySelector("#calc")
+let sinal = false
+let decimal = false
+
+teclasNum.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        if(display.innerHTML == "0"){
+            display.innerHTML = ""
+        }
+        if (evt.target.innerHTML == "."){
+            if (display.innerHTML == ""){
+                display.innerHTML = "0"
+            }
+            if(!decimal){
+                decimal = true
+                sinal = true
+                display.innerHTML += evt.target.innerHTML
+            }
+        } else {
+            if(["-","+","*","/"].includes(display.innerHTML[display.innerHTML.length-1])){
+                decimal = false
+            }
+            display.innerHTML += evt.target.innerHTML
+            sinal = false
+        }
+    })
+})
+teclasOp.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        if (sinal === false){
+            if (display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            if (evt.target.innerHTML == "x") {
+                display.innerHTML += "*"
+            } else {
+                display.innerHTML += evt.target.innerHTML
+            }
+            sinal = true
+            decimal = true
+        } 
+    })
+})
+
+tlimpar.addEventListener("click",()=>{
+    display.innerHTML = "0"
+    decimal = false
+    sinal = false
+})
+
+tigual.addEventListener("click",(evt)=>{
+    sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)
+    display.innerHTML = res
+})
+
+tcopy.addEventListener("click",(evt)=>{
+    navigator.clipboard.writeText(display.innerHTML)
+})
+
+calc_aba.addEventListener("click",(evt)=>{
+    calc.classList.toggle("calc_exibir")
+})
