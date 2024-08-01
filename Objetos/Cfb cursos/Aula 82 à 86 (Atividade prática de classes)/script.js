bf76f932aa45e66891e0168066ab8f10197ee8ba
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-08-01 20:07:51 -0300

    Aula 83

--- Objetos/Cfb cursos/Aula 82 à 86 (Atividade prática de classes)/script.js
+++ Objetos/Cfb cursos/Aula 82 à 86 (Atividade prática de classes)/script.js
@@ -8,3 +8,32 @@ let larguraPalco = palco.offsetWidth
 let alturaPalco = palco.offsetHeight
 let bolas = []
 let numBolas = 0
+
+class Bola {
+    constructor(){
+        this.tam = Math.floor(Math.random()*15)+10
+        this.r = Math.floor(Math.random()*255)
+        this.g = Math.floor(Math.random()*255)
+        this.b = Math.floor(Math.random()*255)
+        this.px = Math.floor(Math.random()*(larguraPalco - this.tam))
+        this.py = Math.floor(Math.random()*(alturaPalco - this.tam))
+    }
+}
+
+window.addEventListener("resize",()=>{
+    larguraPalco = palco.offsetWidth
+    alturaPalco = palco.offsetHeight
+})
+
+btn_add.addEventListener("click",()=>{
+    const qtde = txt_qtde.value
+    for(let i = 0; i<qtde; i++){
+        // Instanciar novas bolinhas
+    }
+})
+
+btn_remover.addEventListener("click",()=>{
+    bolas.map((el)=>{
+        // Removar a bolinha
+    })
+})
