09efcc790f9061d33582412be826e46a82919fbf
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-08-01 21:44:08 -0300

    Aula 86

--- Objetos/Cfb cursos/Aula 82 à 86 (Atividade prática de classes)/script.js
+++ Objetos/Cfb cursos/Aula 82 à 86 (Atividade prática de classes)/script.js
@@ -25,8 +25,8 @@ class Bola {
         this.arrayBolas = arrayBolas
         this.id = Date.now() + "_" + Math.floor(Math.random()*10000000000000)
         this.desenhar()
-        this.controle = setInterval(this.controlar(),10)
         this.eu = document.getElementById(this.id)
+        this.controle = setInterval(this.controlar,10)
         numBolas ++
         num_objetos.innerHTML = numBolas
     }
@@ -50,12 +50,30 @@ class Bola {
         const div = document.createElement("div")
         div.setAttribute("id",this.id)
         div.setAttribute("class","bola")
-        div.setAttribute("style", `left: ${this.px}; top: ${this.py}; width: ${this.tam}; height: ${this.tam}; background-color: rgb(${this.r},${this.g},${this.b});border-radius: 50%`)
+        div.setAttribute("style", `left: ${this.px}px; top: ${this.py}px; width: ${this.tam}px; height: ${this.tam}px; background-color: rgb(${this.r},${this.g},${this.b});`)
         this.palco.appendChild(div)
     }
 
+    controle_bordas = ()=>{
+        if(this.px + this.tam >= larguraPalco){
+            this.dirx = -1
+        } else if(this.px <= 0){
+            this.dirx = 1
+        }
+        if(this.py + this.tam >= alturaPalco){
+            this.diry = -1
+        } else if(this.py <= 0){
+            this.diry = 1
+        }
+    }
     controlar = ()=>{
-
+        this.controle_bordas()
+        this.px += this.dirx*this.velx
+        this.py += this.diry*this.vely
+        this.eu.setAttribute("style", `left: ${this.px}px; top: ${this.py}px; width: ${this.tam}px; height: ${this.tam}px; background-color: rgb(${this.r},${this.g},${this.b});`)
+        if((this.px > larguraPalco) || (this.py > alturaPalco)){
+            this.remover()
+        }
     }
 }
 
@@ -68,11 +86,13 @@ btn_add.addEventListener("click",()=>{
     const qtde = txt_qtde.value
     for(let i = 0; i<qtde; i++){
         // Instanciar novas bolinhas
+        bolas.push(new Bola(bolas, palco))
     }
 })
 
 btn_remover.addEventListener("click",()=>{
     bolas.map((el)=>{
         // Removar a bolinha
+        el.remover()
     })
 })
