a6f223feaa1761d5621d17c01a90c291ac6a2b2d
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-08-06 20:38:00 -0300

    Aula 92

---
+++ Módulos/CFB Cursos/Aula 92 (Módulos pt1)/cursos.js
@@ -0,0 +1,4 @@
+const cursos =  ["Javascript", "HTML", "CSS", "Arduino", "Raspberry", "C++", "Python", "Java", "C#"]
+const carros = ["Polo", "T-Cross", "Golf", "Onix", "Lancer", "Argo", "Cronos"]
+
+export {cursos,carros}
