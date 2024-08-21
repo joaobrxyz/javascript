c344cf08141f1d4ea755c65db7c02ae0b11fea36
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-08-20 21:10:00 -0300

    Aula 101

---
+++ Tratamento de dados/Cfb cursos/Aula 101 (Funções para manipulação de STRING)/script.js
@@ -0,0 +1,18 @@
+let nome = new String("João")
+let canal = new String("CFBCursos")
+
+console.log(nome)
+console.log(typeof(nome))
+
+console.log(nome.charAt(1))
+console.log(nome.charAt(1))
+
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(canal))
+
+console.log(nome)
+
+console.log(nome.indexOf("o"))
+
+console.log(nome.lastIndexOf("o"))
