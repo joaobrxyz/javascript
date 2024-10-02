d5d35b706bd65091db05794299aa658a5b60bc5a
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-10-02 20:28:06 -0300

    Aula 109

--- Eventos DOM/CFB Cursos/Aula 107 a 109 (Animação)/script.js
+++ Eventos DOM/CFB Cursos/Aula 107 a 109 (Animação)/script.js
@@ -11,20 +11,26 @@ let anima = null
 
 const mover = (dir)=>{
     carro.style.left = parseInt(carro.style.left) + (10*dir) + "px"
+    anima = setTimeout(mover,20,dir)
 }
 
 btn_parar.addEventListener("click",()=>{
-    clearInterval(anima)
+    // clearInterval(anima)
+    clearTimeout(anima)
 })
 
 btn_direita.addEventListener("click",()=>{
-    clearInterval(anima)
-    anima = setInterval(mover, 20, 1);
+    // clearInterval(anima)
+    // anima = setInterval(mover, 20, 1);
+    clearTimeout(anima)
+    mover(1)
 })
 
 btn_esquerda.addEventListener("click",()=>{
-    clearInterval(anima)
-    anima = setInterval(mover, 20, -1);
+    // clearInterval(anima)
+    // anima = setInterval(mover, 20, -1);
+    clearTimeout(anima)
+    mover(-1)
 })
 
 window.onload = init
