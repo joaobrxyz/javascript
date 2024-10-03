c60298d91a289184ff03af0f655edbc159837e5e
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-10-03 20:38:35 -0300

    Aula 111

--- Eventos DOM/CFB Cursos/Aula 107 a 111 (Animação)/script.js
+++ Eventos DOM/CFB Cursos/Aula 107 a 111 (Animação)/script.js
@@ -2,16 +2,20 @@ const carro = document.querySelector("#carro")
 const btn_esquerda = document.querySelector("#btn_esquerda")
 const btn_direita = document.querySelector("#btn_direita")
 const parar =  document.querySelector("#btn_parar")
+const rodar =  document.querySelector("#btn_rodar")
 
 const init = ()=>{
-    carro.style = "position:relative;left:0px;width:100px"
-    tamMax = window.innerWidth - parseInt(carro.style.width)
+    carro.style = "position:relative;left:0px;width:100px;height:40px;"
+    tamCarro = parseInt(carro.style.width)
+    tamMax = window.innerWidth - tamCarro
 }
 
 let anima = null
 let tamMax = null
+let tamCarro = null
+let dir = 0
 
-const mover = (dir)=>{
+const mover = (dir)=>{ // Serve pros botões de esquerda e direita
     if(dir > 0) {
         if(parseInt(carro.style.left) <= tamMax){
             carro.style.left = parseInt(carro.style.left) + (10*dir) + "px"
@@ -28,6 +32,20 @@ const mover = (dir)=>{
         }
     }
 }
+
+const move = ()=>{ // Serve para o botão rodar
+    if(parseInt(carro.style.left) >= tamMax){
+        dir = -1
+    } else if(parseInt(carro.style.left) <= 0){
+        dir = 1
+    }
+    carro.style.left = parseInt(carro.style.left) + (10*dir) + "px"
+    anima = setTimeout(move,20)
+}
+
+rodar.addEventListener("click",()=>{
+    move()
+})
 
 btn_parar.addEventListener("click",()=>{
     // clearInterval(anima)
@@ -51,4 +69,16 @@ btn_esquerda.addEventListener("click",()=>{
 window.onload = init
 window.addEventListener("resize",()=>{
     tamMax = window.innerWidth - parseInt(carro.style.width)
+})
+window.addEventListener("keydown",(evt)=>{
+    if(evt.code === "ArrowUp"){
+        carro.style.width = parseInt(carro.style.width) + 10 + "px"
+        carro.style.height = parseInt(carro.style.height) + 10 + "px"
+    }
+    if(evt.code === "ArrowDown"){
+        carro.style.width = parseInt(carro.style.width) - 10 + "px"
+        carro.style.height = parseInt(carro.style.height) - 10 + "px"
+    }
+    tamCarro = parseInt(carro.style.width)
+    tamMax = window.innerWidth - tamCarro
 })
